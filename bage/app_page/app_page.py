ce90ee6295e08f0d01a85c936c950351176e5818
Tone-one <1191461802@qq.com>
2021-09-18 16:29:25 +0800

    9-18

---
+++ bage/app_page/app_page.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/7 11:25
+# @Author  : A one
+
+"""
+driver界面
+
+"""
+
+
+class App:
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def goto_main(self):
+        return Mainpage()
---
+++ bage/app_page/main_page.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/18 9:49
+# @Author  : A one
+
+class MainPage:
+    def goto_addressList(self):
+
